ab90b9a1b243ffdf1ca753ed226b7cdb1ba55ede
unknown <ealcan.v@gmail.com>
2024-05-19 16:59:49 +0200

    Rewards y Wins

--- vite-project-CopiaSeguridad/src/components/common/ButtonSubs.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-const ButtonSubs: FC = () => {
-    return (
-        <a href="/register" className="waves-effect waves-light btn-large colorbase"><i className="material-icons right">add_circle</i>Suscríbete</a>
-    )
-}
-
-export default ButtonSubs;
